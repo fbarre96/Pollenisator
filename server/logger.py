21a76d9ee107d633ed128609b23a9ed760a54844
fbarre96 <fbarre96@hotmail.fr>
2021-04-23 15:05:22 +0200

    fix logs writting

--- server/logger.py
+++ server/logger.py
@@ -1,9 +1,10 @@
 import os
 from datetime import datetime
+
 def report(body):
     err = body.get("error", "")
     if err != "":
         logs_folder = os.path.join(os.path.dirname(
             os.path.realpath(__file__)), "./logs/clients/error.log")
         with open(logs_folder, "a+") as f:
-            f.write(datetime.now()+": "+err)
+            f.write(str(datetime.now())+": "+err)
