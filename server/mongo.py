088c7957327763a2f4250fdf29b468681c0cbc1b
fbarre96 <fbarre96@hotmail.fr>
2021-06-09 09:31:09 +0200

    Fix bug in db creation

--- server/mongo.py
+++ server/mongo.py
@@ -318,8 +318,8 @@ def prepareCalendar(dbName, pentest_type, start_date, end_date, scope, settings,
     insert_wave(dbName, WaveController(wave_o).getData(), **kwargs)
     interval_o = ServerInterval().initialize(dbName, start_date, end_date)
     insert_interval(dbName, IntervalController(interval_o).getData(), **kwargs)
-    scopes = scopes.replace("https://", "").replace("http://","")
-    scopes = scopes.replace("\n", ",").split(",")
+    scope = scope.replace("https://", "").replace("http://","")
+    scope = scope.replace("\n", ",").split(",")
     for scope_item in scopes:
         if scope_item.strip() != "":
             if isIp(scope_item.strip()):
