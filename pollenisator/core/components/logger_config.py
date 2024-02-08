b1743c78a13ce299483bd3bf80494a7a1bfca44e
fbarre96 <fbarre96@hotmail.fr>
2024-02-08 10:41:25 +0100

    comments for logger

--- pollenisator/core/components/logger_config.py
+++ pollenisator/core/components/logger_config.py
@@ -1,6 +1,8 @@
-
+"""Logger configuration for the application."""
 import logging
 import sys
+
+
 logging.basicConfig(filename='debug.log', encoding='utf-8', level=logging.DEBUG)
 file_handler_error = logging.FileHandler('error.log', mode='w')
 file_handler_error.setLevel(logging.ERROR)
@@ -9,7 +11,14 @@ handler = logging.StreamHandler(stream=sys.stdout)
 logger.addHandler(handler)
 logger.addHandler(file_handler_error)
 
-def handle_exception(exc_type, exc_value, exc_traceback):
+def handle_exception(exc_type, exc_value, exc_traceback) -> None:
+    """function to handle uncaught exceptions and log them to the error.log file.
+    Args:
+        exc_type (Exception): The type of the exception.
+        exc_value (Exception): The value of the exception.
+        exc_traceback (Exception): The traceback of the exception.
+    """
+
     logger.error("Exception", exc_info=(exc_type, exc_value, exc_traceback))
     if issubclass(exc_type, KeyboardInterrupt):
         sys.__excepthook__(exc_type, exc_value, exc_traceback)
